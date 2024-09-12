6b51136764642e2414aa5fc1d361353b1b656dba
Eduardo de Oliveira <109693585+eduoliveira1504@users.noreply.github.com>
2024-09-12 15:29:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ st.info('This is app builds a machine learning model!')
 
 with st.expander('Data'):
   st.write('**Raw data**')
-  df = pd.read_csv('penguins_cleaned.csv')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
 
   st.write('**X**')
